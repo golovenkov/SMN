aa35fd608e624bd27d469feceb8b9cf4634fe2f7
user1 <user1@user.domain>
2018-08-20 12:10:30 +0300

    Added SAN implementation v1, very slow

--- model_san.py
+++ model_san.py
@@ -1,10 +1,10 @@
 import numpy as np
 np.random.seed(42)
 
-from keras.layers import Conv2D, MaxPooling2D, Embedding, GRU
+from keras.layers import Conv2D, MaxPooling2D, Embedding, GRU, Reshape
 from keras.layers import TimeDistributed
 from keras.layers import Dense, Input, Flatten, Lambda, Softmax, Dot
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.engine.topology import Layer
 from keras import backend as K
 from keras import initializers
@@ -32,36 +32,61 @@ class StaticWeights(Layer):
         return (input_shape[0], self.last_dim)
 
 
-def build_SMN(max_turn, maxlen, word_dim, sent_dim, last_dim, num_words, embedding_matrix):
+def build_SAN(max_turn, maxlen, word_dim, sent_dim, last_dim, num_words, embedding_matrix):
 
-    def MatchByWords(max_turn=10):
+    def AttentionBlock(word_dim=200, sent_dim=200, maxlen=50):
         def inside(args):
-            u = args[0]  # utterances Tensor
-            r = args[1]  # response Tensor
-            # concatenate all max_turn (10) M_1 word-word similarity matrices (50x50)
-            return Lambda(lambda x: K.stack(x, axis=1))(
-                [
-                    # dot product by the last axis (embeddings dimension)
-                    Dot(axes=(-1, -1))(
-                        [(Lambda(lambda x: x[:, turn])(u)), r]
-                    ) for turn in range(max_turn)
-                ]
-            )
+            e_u = args[0]  # word-level an utterance representation (?,  50, 200)
+            e_r = args[1]  # word-level representation of a word in a response   (?, 200)
+            h_u = args[2]  # segment-level an utterance representation (?,  50, 200)
+            h_r = args[3]  # segment-level representation of a segment in a response (?, 200)
+            t1 = Dense(word_dim, trainable=True, use_bias=True, kernel_initializer=initializers.glorot_uniform())(e_u)
+            t1 = Dot(axes=(-1, -1))([t1, e_r])
+            t1 = Lambda(lambda x: K.tanh(x))(t1)
+            t1 = Dense(maxlen)(t1)    # (?, 50)
+            t1 = Softmax(axis=1)(t1)
+            t1 = Lambda(lambda x: tf.multiply(tf.expand_dims(x[0], axis=-1), x[1]))([t1, e_u])  # "weight" the utterance vector according to the response
+            t1 = Lambda(lambda x: tf.reduce_sum(x, axis=1))(t1)
+            t1 = Lambda(lambda x: tf.multiply(x[0], x[1]))([t1, e_r])  # Hadamard product to the response vector
+
+            t2 = Dense(sent_dim, trainable=True, use_bias=True, kernel_initializer=initializers.glorot_uniform())(h_u)
+            t2 = Dot(axes=(-1, -1))([t2, h_r])
+            t2 = Lambda(lambda x: K.tanh(x))(t2)
+            t2 = Dense(maxlen)(t2)  # (?, 50)
+            t2 = Softmax(axis=1)(t2)
+            t2 = Lambda(lambda x: tf.multiply(tf.expand_dims(x[0], axis=-1), x[1]))([t2, h_u])  # "weight" the utterance vector according to the response
+            t2 = Lambda(lambda x: tf.reduce_sum(x, axis=1))(t2)
+            t2 = Lambda(lambda x: tf.multiply(x[0], x[1]))([t2, h_r])  # Hadamard product to the response vector
+
+            # t = K.concatenate([t1, t2], axis=-1)  # concatenated vector t
+            t = Lambda(lambda x: K.stack(x, axis=-1))([t1, t2])
+            t = Reshape((2 * sent_dim,))(t)
+            return t
         return inside
 
-    def MatchBySegments(sent_dim=200, max_turn=10):
+    def WordsAndRepresentationsAttention(maxlen=50, max_turn=10):
         def inside(args):
-            u = args[0]  # utterances Tensor
-            r = args[1]  # response Tensor
-            return Lambda(lambda x: K.stack(x, axis=1))(
+            word_u = args[0]  # word-level utterances Tensor (?, 10, 50, 200)
+            word_r = args[1]  # word-level response Tensor   (?, 50, 200)
+            segm_u = args[2]  # segment-level utterances Tensor (?, 10, 50, 200)
+            segm_r = args[3]  # segment-level utterances Tensor (?, 50, 200)
+
+            T = Lambda(lambda x: K.stack(x, axis=1))(      # for each (utterance-response) pair
                 [
-                    # dot product by the last axis (embeddings dimension)
-                    Dot(axes=(-1,-1))([
-                        # Dense(sent_dim, use_bias=False, trainable=True)(Lambda(lambda x: x[:, turn])(u)), r]
-                        Dense(sent_dim, use_bias=False, trainable=True, kernel_initializer=initializers.glorot_uniform())(Lambda(lambda x: x[:, turn])(u)), r]
+                    Lambda(lambda x: K.stack(x, axis=1))(  # for each word/segment in response Vector
+                        [
+                            AttentionBlock()([
+                                Lambda(lambda x: x[:, turn])(word_u),
+                                Lambda(lambda x: x[:, word])(word_r),
+                                Lambda(lambda x: x[:, turn])(segm_u),
+                                Lambda(lambda x: x[:, word])(segm_r)
+                            ]
+                            ) for word in range(maxlen)
+                        ]
                     ) for turn in range(max_turn)
                 ]
             )
+            return T
         return inside
 
 
@@ -116,24 +141,21 @@ def build_SMN(max_turn, maxlen, word_dim, sent_dim, last_dim, num_words, embeddi
     context_sent_embedding = TimeDistributed(sentence2vec)(context_word_embedding)
     response_sent_embedding = sentence2vec(response_word_embedding)
 
-    word_match = MatchByWords()([context_word_embedding, response_word_embedding])
-    segm_match = MatchBySegments()([context_sent_embedding, response_sent_embedding])
-    match_2ch = Lambda(lambda x: K.stack([x[0], x[1]], axis=2))([word_match, segm_match])   # (?, 10, 2, 50, 50) M_1 & M_2 as 2 channels
-
-    conv = TimeDistributed(Conv2D(8, (3, 3), activation='relu', data_format='channels_first', kernel_initializer=initializers.he_normal()))(match_2ch)
-    pool = TimeDistributed(MaxPooling2D(pool_size=(3, 3), data_format='channels_first'))(conv)
-    flat = TimeDistributed(Flatten())(pool)
-    match = TimeDistributed(Dense(last_dim, activation='tanh', kernel_initializer=initializers.glorot_uniform()))(flat)  # v
+    # Attention Interaction Aggregation
+    T = WordsAndRepresentationsAttention()([context_word_embedding, response_word_embedding,
+                                                context_sent_embedding, response_sent_embedding])
+    gru2 = GRU(last_dim, return_sequences=False, kernel_initializer=initializers.orthogonal())
+    v = TimeDistributed(gru2)(T)
 
     ##############################################################################################
     # SMN last
-    # output_last = GRU(last_dim, kernel_initializer=initializers.orthogonal())(match)
-    # output = Dense(1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform())(output_last)  # DMN_last
+    output_last = GRU(last_dim, kernel_initializer=initializers.orthogonal())(v)
+    output = Dense(1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform())(output_last)  # DMN_last
     ##############################################################################################
 
     ##############################################################################################
     # SMN static
-    # out_static = GRU(last_dim, return_sequences=True, kernel_initializer=initializers.orthogonal())(match)
+    # out_static = GRU(last_dim, return_sequences=True, kernel_initializer=initializers.orthogonal())(v)
     # out_static = StaticWeights(last_dim=last_dim, max_turn=max_turn)(out_static)
     # output = Dense(1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform())(out_static)  # DMN_static
     ##############################################################################################
@@ -141,8 +163,8 @@ def build_SMN(max_turn, maxlen, word_dim, sent_dim, last_dim, num_words, embeddi
     ##############################################################################################
     # SMN dynamic
     # attention over hidden states h'
-    output_dynamic = DynamicAttention()([context_sent_embedding, match])   # (?, q)
-    output = Dense(1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform())(output_dynamic)
+    # output_dynamic = DynamicAttention()([context_sent_embedding, v])   # (?, q)
+    # output = Dense(1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform())(output_dynamic)
     ##############################################################################################
 
     model = Model(inputs=[context_input, response_input], outputs=[output])
---
+++ train_san.py
@@ -0,0 +1,78 @@
+import argparse
+import numpy as np
+np.random.seed(42)
+
+import joblib
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+import tensorflow as tf
+tf.set_random_seed(42)
+from keras import backend as K
+from model_san import build_SAN
+
+
+def main():
+    # TensorFlow wizardry
+    config = tf.ConfigProto()
+
+    # Don't pre-allocate memory; allocate as-needed
+    config.gpu_options.allow_growth = True
+
+    # # Only allow a total of half the GPU memory to be allocated
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.3
+
+    # Create a session with the above options specified.
+    K.tensorflow_backend.set_session(tf.Session(config=config))
+
+    psr = argparse.ArgumentParser()
+    psr.add_argument('--maxlen', default=50, type=int)
+    psr.add_argument('--max_turn', default=10, type=int)
+    psr.add_argument('--num_words', default=50000, type=int)
+    psr.add_argument('--word_dim', default=200, type=int)
+    psr.add_argument('--sent_dim', default=200, type=int)
+    psr.add_argument('--last_dim', default=50, type=int)
+    psr.add_argument('--embedding_matrix', default='embedding_matrix.joblib')
+    psr.add_argument('--train_data', default='train.joblib')
+    psr.add_argument('--valid_data', default='valid.joblib')
+    psr.add_argument('--model_name', default='SMN_last')
+    psr.add_argument('--batch_size', default=512, type=int)
+    args = psr.parse_args()
+
+    print('load embedding matrix')
+    embedding_matrix = joblib.load(args.embedding_matrix)
+
+    print('build model')
+    model = build_SAN(args.max_turn, args.maxlen, args.word_dim, args.sent_dim, args.last_dim, args.num_words, embedding_matrix)
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    print(model.summary())
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+    model_checkpoint = ModelCheckpoint(args.model_name + '.h5', save_best_only=True, save_weights_only=True)
+
+    print('load train data')
+    train_data = joblib.load(args.train_data)
+    context = np.array(train_data['context'])
+    response = np.array(train_data['response'])
+    labels = train_data['labels']
+
+    print('load valid data')
+    valid_data = joblib.load(args.valid_data)
+    valid_context = np.array(valid_data['context'])
+    valid_response = np.array(valid_data['response'])
+    valid_labels = valid_data['labels']
+
+    print('fitting')
+    model.fit(
+        [context, response],
+        labels,
+        validation_data=([valid_context, valid_response], valid_labels),
+        batch_size=args.batch_size,
+        epochs=10,
+        callbacks=[early_stopping, model_checkpoint]
+    )
+
+if __name__ == '__main__': main()
